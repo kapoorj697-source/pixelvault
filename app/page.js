e8959543366c27006ad4610edf28cc2bbc4e2bb7
kapoorj697-source <kapoorj697@gmail.com>
2026-02-18 19:25:28 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding:40,
+      fontFamily:"sans-serif",
+      textAlign:"center"
+    }}>
+      <h1>PixelVault</h1>
+      <p>Your gallery is live 🚀</p>
+    </div>
+  );
+}
