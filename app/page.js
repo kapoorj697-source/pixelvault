5fc8bb84d3b5c0fbbf97c50419d3c82af2624213
kapoorj697-source <kapoorj697@gmail.com>
2026-02-19 11:53:15 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <main style={{minHeight:"100vh",display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
       <h1>PixelVault</h1>
       <p>Home live ✅</p>
-      <a href="/test" style={{color:"#7c3aed"}}>Open Gallery / Test Page</a>
+      <a href="/gallery" style={{color:"#7c3aed"}}>Open Gallery / Test Page</a>
     </main>
   );
 }
